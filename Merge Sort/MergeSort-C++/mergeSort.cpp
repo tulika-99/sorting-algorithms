3ed87f40081a35ddbeaf6a4543f0f6b169360011
Akshay Kharbanda <akshaykharbanda.ak99@gmail.com>
2019-10-03 02:41:17 +0530

    Added merge sort algorithm in C++

---
+++ Merge Sort/MergeSort-C++/mergeSort.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void merge(int *a, int low, int high, int mid)
+{
+    int i, j, k, temp[high-low+1];
+    i = low;
+    k = 0;
+    j = mid + 1;
+ 
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+    }
+ 
+    while (i <= mid)
+        temp[k++] = a[i++];
+ 
+    while (j <= high)
+        temp[k++] = a[j++];
+ 
+ 
+    for (i = low; i <= high; i++)
+        a[i] = temp[i-low];
+}
+ 
+void MergeSort(int *a, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid=(low+high)/2;
+
+        MergeSort(a, low, mid);
+        MergeSort(a, mid+1, high);
+ 
+        merge(a, low, high, mid);
+    }
+}
+ 
+int main()
+{
+    int n, i;
+    cout<<"\nEnter the number of data element to be sorted: ";
+    cin>>n;
+ 
+    int arr[n];
+    for(i = 0; i < n; i++)
+    {
+        cout<<"Enter element "<<i+1<<": ";
+        cin>>arr[i];
+    }
+ 
+    MergeSort(arr, 0, n-1);
+ 
+    cout<<"\nSorted Data ";
+    for (i = 0; i < n; i++)
+        cout<<"->"<<arr[i];
+ 
+    return 0;
+}
