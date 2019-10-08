fd686c2ee342468c9a4de62ea1a1b254751b003a
Archie B. Reyes <areyes@inovotek.com>
2019-10-08 14:20:32 +0800

    Add heapsort for javascript

---
+++ Heap Sort/HeapSort.js
@@ -0,0 +1,44 @@
+let array_length;
+/* to create MAX  array */
+const heap_root = (input, i) => {
+    var left = 2 * i + 1;
+    var right = 2 * i + 2;
+    var max = i;
+
+    if (left < array_length && input[left] > input[max]) {
+        max = left;
+    }
+
+    if (right < array_length && input[right] > input[max]) {
+        max = right;
+    }
+
+    if (max != i) {
+        swap(input, i, max);
+        heap_root(input, max);
+    }
+}
+
+const swap = (input, index_A, index_B) => {
+    var temp = input[index_A];
+    input[index_A] = input[index_B];
+    input[index_B] = temp;
+}
+
+const heapSort = (input) => {
+    array_length = input.length;
+
+    for (var i = Math.floor(array_length / 2); i >= 0; i -= 1) {
+        heap_root(input, i);
+    }
+
+    for (i = input.length - 1; i > 0; i--) {
+        swap(input, 0, i);
+        array_length--;
+        heap_root(input, 0);
+    }
+}
+
+let arr = [3, 0, 2, 5, -1, 4, 1];
+heapSort(arr);
+console.log(arr);
