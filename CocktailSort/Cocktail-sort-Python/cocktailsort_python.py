756bdff0e3f1a2fa1221c83fe148e57149b08e4c
Limin <liminmalek@gmail.com>
2019-10-13 00:09:08 +0200

    Added folder for cocktail sort python

---
+++ CocktailSort/Cocktail-sort-Python/cocktailsort_python.py
@@ -0,0 +1,24 @@
+def cocktailSort(array):
+    arrayLen = len(array)
+    check = 1
+    while(check == 1):
+        check = 0
+        for x in range(arrayLen-1):
+            if (array[x] > array[x+1]):
+                check = 1
+                array[x], array[x+1] = array[x+1], array[x]
+            if (x == arrayLen - 1):
+                break
+        if (check == 1):
+            check = 0
+            for y in reversed(range(arrayLen -1)):
+                if array[y] > array[y+1]:
+                    check = 1
+                    array[y], array[y+1] = array[y+1], array[y]
+                if (y == 0):
+                    break
+
+numbers = [3,45,1,3,2,8,5,0]
+print("Unsorted Numbers: ", numbers)
+cocktailSort(numbers)
+print("Sorted Numbers: ", numbers)
