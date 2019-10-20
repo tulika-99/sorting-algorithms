691ca824233c465407f8a1f307a17f7337a5f368
werewolf-spirit <55404781+werewolf-spirit@users.noreply.github.com>
2019-10-20 21:13:38 +0530

    Added comments

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,3 +1,4 @@
+//header files
 #include<stdio.h>
 #include<string.h>
 void main()
@@ -6,6 +7,7 @@ void main()
 	char name[50][50],temp[50];
 	printf("Enter how many names:\t");
 	scanf("%d",&n);
+	//reading names
 	printf("Enter the names\n");
 	for(i=0;i<n;i++)
 	{
@@ -13,11 +15,12 @@ void main()
 		printf("Name %d\t",i+1);
 		gets(name[i]);
 	}
+	//comparing names and sorting them
 	for(i=1;i<n;i++)
 	{
 		for(j=0;j<n-i;j++)
 		{
-			if(strcmp(name[j],name[j+1])>0)
+			if(strcmp(name[j],name[j+1])>0)//string function
 			{
 				strcpy(temp,name[j]);
 				strcpy(name[j],name[j+1]);
@@ -25,6 +28,7 @@ void main()
 			}
 		}
 	}
+	//printing sorted array
 	printf("The names in order\n");
 	for(i=0;i<n;i++)
 	{
