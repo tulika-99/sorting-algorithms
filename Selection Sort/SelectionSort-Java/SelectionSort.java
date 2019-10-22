2981f7e86e6cf823ea94b2785db34ee783d4dbb6
Febri D. Rianto <44501367+Blazing21@users.noreply.github.com>
2019-10-22 17:19:53 +0800

    Update SelectionSort.java

--- Selection Sort/SelectionSort-Java/SelectionSort.java
+++ Selection Sort/SelectionSort-Java/SelectionSort.java
@@ -1,22 +1,40 @@
-public class SelectionSort {
-    public static int[] selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-
-            // find the index of the smallest value
-            int min_index = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                // if values after i are smaller, replace min_index
-                if (arr[j] < arr[min_index]) {
-                    min_index = j;
-                }
-            }
-
-            // swap the smallest value with number at i
-            int temp = arr[min_index];
-            arr[min_index] = arr[i];
-            arr[i] = temp;
-        }
-
-        return arr;
-    }
+class SelectionSort { 
+    
+    void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+  
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    } 
+  
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+  
+        System.out.println(); 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        int arr[] = { 12, 11, 13, 5, 6 }; 
+        InsertionSort ob = new InsertionSort(); 
+        System.out.println("Selection Sorting");
+        System.out.print("Before Sorting : ");
+        for(int x = 0; x < 5; x++)
+            System.out.print(arr[x]+" ");
+        ob.sort(arr); 
+        System.out.println("");
+        System.out.print("After Sorting : ");
+        printArray(arr); 
+    } 
 }
