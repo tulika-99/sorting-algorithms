cb0bc2db4abaf2e204b8d46833c211e3baea99f3
BrijeshSinghRawat <123789b@gmail.com>
2020-10-04 18:11:49 +0530

    Improving the performance of bubble sort.
    Earlier Bubble Sort Algorithm was doing unnecessary iterations even after the array was already sorted. In this Solution we will break the iteration if after one full Iteration it doesn't find any values to swap, which means Array is already Sorted.
    
    Number of Iterations Before Changes:
    ========================
    [5, 2, 9, 1, 7, 3, 4, 505, 200]
    [2, 5, 1, 7, 3, 4, 9, 200, 505]
    [2, 1, 5, 3, 4, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]
    
    Number of Iterations After Changes:
    =======================
    [5, 2, 9, 1, 7, 3, 4, 505, 200]
    [2, 5, 1, 7, 3, 4, 9, 200, 505]
    [2, 1, 5, 3, 4, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]
    [1, 2, 3, 4, 5, 7, 9, 200, 505]

--- Bubble Sort/BubbleSort-Java/BubbleSort.java
+++ Bubble Sort/BubbleSort-Java/BubbleSort.java
@@ -3,7 +3,9 @@ public class BubbleSort
     void bubbleSort(int arr[])
     {
         int n = arr.length;
-        for (int i = 0; i < n-1; i++)
+        boolean swapped;
+        for (int i = 0; i < n-1; i++){
+            swapped=false;
             for (int j = 0; j < n-i-1; j++)
                 if (arr[j] > arr[j+1])
                 {
@@ -11,7 +13,12 @@ public class BubbleSort
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped=true;
                 }
+            if(!swapped){
+		    break;
+		}
+        }
     }
 
     /* Prints the array */
